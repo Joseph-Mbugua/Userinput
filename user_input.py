9586a478cf32cdcf5ed1a242075578e4b5668f8c
josephmbugua968 <31985047-josephmbugua968@users.noreply.replit.com>
2024-02-21 22:01:11 +0000

    Initial commit

---
+++ user_input.py
@@ -0,0 +1,13 @@
+# user_input.py
+
+# Ask user for name 
+name = input("What is your name? ")
+
+# Ask user for age 
+age = input("How old are you? ")
+
+# Ask user for location
+location = input("Where are you located? ")
+
+# Print out a personalized message using the user's name, age, and location
+print("Hello {}, you are {} years old and you live in {}.".format(name, age, location))
