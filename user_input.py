4272e88da24c8af2f6c398e9e866411c88c85080
Joseph-Mbugua <160980178+Joseph-Mbugua@users.noreply.github.com>
2024-02-23 00:44:49 +0000

    commit

--- user_input.py
+++ user_input.py
@@ -1,13 +1,14 @@
 # user_input.py
 
-# Ask user for name 
+# Ask user for name
 name = input("What is your name? ")
 
-# Ask user for age 
+# Ask user for age
 age = input("How old are you? ")
 
 # Ask user for location
 location = input("Where are you located? ")
 
 # Print out a personalized message using the user's name, age, and location
-print("Hello {}, you are {} years old and you live in {}.".format(name, age, location))
+print("Hello {}, you are {} years old and you live in {}.".format(
+    name, age, location))
